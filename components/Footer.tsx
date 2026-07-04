391ee50aec437192f2683c1f77a6b0ddf3ebc15b
Biswaranjan Pradhan <biswaranjanpradhan287@gmail.com>
2026-07-04 09:32:36 +0530

    update copyright year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,7 @@ const Footer = () => {
             </div>
             <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
                 <p className="md:text-base text-sm md:font-normal font-light">
-                    Copyright © 2025 Biswaranjan Pradhan
+                    Copyright © 2026 Biswaranjan Pradhan
                 </p>
 
 
